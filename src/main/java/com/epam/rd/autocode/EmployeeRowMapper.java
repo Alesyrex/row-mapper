4f200f44957ac9f84f58d5c96403abb0ae5e2018
Alesyrex <Alesyrex@gmail.com>
2022-05-10 13:55:39 +0300

    add constant,remove throws

--- src/main/java/com/epam/rd/autocode/EmployeeRowMapper.java
+++ src/main/java/com/epam/rd/autocode/EmployeeRowMapper.java
@@ -13,22 +13,30 @@ import java.util.logging.Logger;
 
 
 public class EmployeeRowMapper implements RowMapper<Employee> {
+    public static final String FIRSTNAME = "firstname";
+    public static final String LASTNAME = "lastname";
+    public static final String MIDDLENAME = "middlename";
+    public static final String POSITION = "position";
+    public static final String HIREDATE = "hiredate";
+    public static final String ID = "id";
+    public static final String SALARY = "salary";
+    public static final String EXCEPTION = "Exception: ";
+
     private static final Logger LOGGER = Logger.getLogger(EmployeeRowMapper.class.getName());
 
     @Override
-    public Employee mapRow(ResultSet resultSet) throws SQLException {
-        Employee employee;
+    public Employee mapRow(ResultSet resultSet) {
+        Employee employee = null;
         try {
-            FullName fullName = new FullName(resultSet.getString("firstname"),
-                    resultSet.getString("lastname"), resultSet.getString("middlename"));
-            Position position = Position.valueOf(resultSet.getString("position"));
-            LocalDate hired = resultSet.getDate("hiredate").toLocalDate();
-            BigInteger id = resultSet.getBigDecimal("id").toBigInteger();
+            FullName fullName = new FullName(resultSet.getString(FIRSTNAME),
+                    resultSet.getString(LASTNAME), resultSet.getString(MIDDLENAME));
+            Position position = Position.valueOf(resultSet.getString(POSITION));
+            LocalDate hired = resultSet.getDate(HIREDATE).toLocalDate();
+            BigInteger id = resultSet.getBigDecimal(ID).toBigInteger();
             employee = new Employee(id, fullName, position,
-                    hired, resultSet.getBigDecimal("salary"));
+                    hired, resultSet.getBigDecimal(SALARY));
         } catch (SQLException sqlEx) {
-            LOGGER.log(Level.SEVERE, "Exception: ", sqlEx);
-            throw sqlEx;
+            LOGGER.log(Level.SEVERE, EXCEPTION, sqlEx);
         }
         return employee;
     }
